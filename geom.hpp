bb2e994b0bba6480e85cf488c89cb63466b3a5db
Lacho <61554888+ZariProjects@users.noreply.github.com>
2021-11-13 23:30:04 +0200

    Finish (hopefully)

--- geom.hpp
+++ geom.hpp
@@ -2,71 +2,87 @@
 #include <cmath>
 using namespace std;
 
-bool eq(double d1, double d2, double eps = 0.001) {
+bool eq(double d1, double d2, double eps = 0.001)
+{
     return abs(d2 - d1) < eps;
 }
 
-struct Point {
+struct Point
+{
     double x, y;
 
     Point(double x = 0, double y = 0) : x(x), y(y) {}
 
-    void print(ostream& out) const {
-        out<< "("<< x<< ", "<< y<< ")";
+    void print(ostream &out) const
+    {
+        out << "(" << x << ", " << y << ")";
     }
 };
 
-ostream& operator<<(ostream& out, const Point& p) {
+ostream &operator<<(ostream &out, const Point &p)
+{
     p.print(out);
     return out;
 }
 
-istream& operator>>(istream& in, Point& p) {
+istream &operator>>(istream &in, Point &p)
+{
     in >> p.x;
     in >> p.y;
-    
+
     return in;
 }
 
-struct Line {
+struct Line
+{
     double A, B, C;
 
     Line(double A = 0, double B = 0, double C = 0) : A(A), B(B), C(C) {}
 
-    Line(const Point& p1, const Point& p2) {
-        A = 0; B = 0; C = 0;
+    Line(const Point &p1, const Point &p2)
+    {
+
+        A = p1.y - p2.y;
+        B = p2.x - p1.x;
+        C = p1.y * p2.x - p1.x * p2.y;
+    }
+    bool parallel(const Line &other) const
+    {
+        return eq(this->A * other.B, this->B * other.A) && this->A * other.C != other.A * this->C;
     }
 
-    bool parallel(const Line& other) const {
-        return true;
+    Line parallel(const Point &p)
+    {
+        return Line(this->A, this->B, -(this->A * p.x + this->B * p.y));
     }
 
-    Line parallel(const Point& p) {
-        return Line(0,0,0);
+    bool perpendicular(const Line &other) const
+    {
+        return eq(this->A * other.A, -(this->B * other.B));
     }
 
-    bool perpendicular(const Line& other) const {
-        return true;
+    Line perpendicular(const Point &p)
+    {
+        return Line(-this->B, this->A, this->B * p.x - this->A * p.y);
     }
 
-    Line perpendicular(const Point& p) {
-        return Line(0,0,0);
-    }
-
-    void print(ostream& out) const {
-        out<< A << "x + "<< B<< "y + "<< C<< " = 0";
+    void print(ostream &out) const
+    {
+        out << A << "x + " << B << "y + " << C << " = 0";
     }
 };
 
-ostream& operator<<(ostream& out, const Line& l) {
+ostream &operator<<(ostream &out, const Line &l)
+{
     l.print(out);
     return out;
 }
 
-istream& operator>>(istream& in, Line& l) {
+istream &operator>>(istream &in, Line &l)
+{
     in >> l.A;
     in >> l.B;
     in >> l.C;
-    
+
     return in;
 }
